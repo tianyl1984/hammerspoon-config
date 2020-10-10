1a1f89a03b3be60c8cd554c40e3b801542922878
tianyl <tianyl1984@qq.com>
2020-10-10 17:30:38 +0800

    add

--- init.lua
+++ init.lua
@@ -1,1 +1,21 @@
 require 'hammerspoon-config.ip.ip'
+
+-- function demo()
+-- 	print('f is press')
+-- 	hs.window.highlight.ui.overlay=true
+-- 	hs.window.highlight.ui.overlayColor = {0.2,0.05,0,0.25}
+-- 	hs.window.highlight.ui.overlayColorInverted = {0.8,0.9,1,0.3}
+-- 	hs.window.highlight.ui.isolateColor = {0.2,0,0,0.95}
+-- 	hs.window.highlight.ui.isolateColorInverted = {1,1,1,0.95}
+-- 	hs.window.highlight.ui.frameWidth = 10
+-- 	hs.window.highlight.ui.frameColor = {0,0.6,1,0.5}
+-- 	hs.window.highlight.ui.frameColorInvert = {1,0.4,0,0.5}
+-- 	hs.window.highlight.ui.flashDuration = 0.8 
+-- 	hs.window.highlight.ui.windowShownFlashColor = {0,1,0,0.8}
+-- 	hs.window.highlight.ui.windowHiddenFlashColor = {1,0,0,0.8}
+-- 	hs.window.highlight.ui.windowShownFlashColorInvert = {1,0,1,0.8}
+-- 	hs.window.highlight.ui.windowHiddenFlashColorInvert = {0,1,1,0.8}
+-- 	hs.window.highlight.toggleIsolate(true)
+-- end
+
+-- hs.hotkey.bind('ctrl', 'z', '', demo)
